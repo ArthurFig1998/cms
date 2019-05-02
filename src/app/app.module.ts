a44d413c78045a49b0647081f15dab4d1cf96b5d
Arthur Figueiredo <fig16009@byui.edu>
2019-05-02 11:56:04 -0600

    xfhm ,cuxctyu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactsDetailComponent } from './contacts/contacts-detail/contacts-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactsComponent,
+    ContactListComponent,
+    ContactsDetailComponent
   ],
   imports: [
     BrowserModule
