041ddcf15d3790f1c628ed03788047582b413bee
Arthur Figueiredo <fig16009@byui.edu>
2019-05-04 19:30:09 -0600

    kjhvkufguy

--- src/app/contacts/contacts-detail/contacts-detail.component.ts
+++ src/app/contacts/contacts-detail/contacts-detail.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Contact } from "../contacts.model";
 
 @Component({
-  selector: 'cms-contacts-detail',
-  templateUrl: './contacts-detail.component.html',
-  styleUrls: ['./contacts-detail.component.css']
+  selector: "cms-contacts-detail",
+  templateUrl: "./contacts-detail.component.html",
+  styleUrls: ["./contacts-detail.component.css"]
 })
 export class ContactsDetailComponent implements OnInit {
+  contact: Contact[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
