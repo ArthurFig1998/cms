d134040d76daf9aceec7b6477289fafa21388701
Arthur Figueiredo <fig16009@byui.edu>
2019-05-15 16:01:44 -0600

    week 04

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Document } from "../document.model";
 
 @Component({
-  selector: 'cms-document-list',
-  templateUrl: './document-list.component.html',
-  styleUrls: ['./document-list.component.css']
+  selector: "cms-document-list",
+  templateUrl: "./document-list.component.html",
+  styleUrls: ["./document-list.component.css"]
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
-  constructor() { }
+  documents: Document[] = [
+    new Document(
+      "1",
+      "CIT 260 - Objected Oriented Programming",
+      "description2",
+      "url2",
+      null
+    ),
+    new Document(
+      "2",
+      "CIT 366 - Full Web Stack Development",
+      "Learn how to develop modern web applications using the MEAN stack",
+      "https://content.byui.edu/file/",
+      null
+    ),
+    new Document(
+      "3",
+      "CIT 425 - Data Warehousing",
+      "description3",
+      "url3",
+      null
+    ),
+    new Document(
+      "4",
+      "CIT 460 - Enterprise Development",
+      "description2",
+      "url2",
+      null
+    ),
+    new Document(
+      "5",
+      "CIT 495 - Senior Practicum",
+      "description3",
+      "url3",
+      null
+    )
+  ];
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
-
 }
--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'cms-documents',
-  templateUrl: './documents.component.html',
-  styleUrls: ['./documents.component.css']
+  selector: "cms-documents",
+  templateUrl: "./documents.component.html",
+  styleUrls: ["./documents.component.css"]
 })
 export class DocumentsComponent implements OnInit {
+  selectedDocument: Document;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
